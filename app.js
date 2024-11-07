99d354671d21fa8d0a7479d6c9f73ebfe57e2bef
WELLINGTON RODRIGO CRUZ <wellington.rcruz@senacsp.edu.br>
2024-11-07 20:47:11 -0300

    quebrar lâmpada

--- app.js
+++ app.js
@@ -4,11 +4,12 @@
  */
 
 function quebrar() {
-    document.getElementById('lamp').src="img/broken.jpg"
+   document.getElementById('lamp').src="img/broken.jpg"
     // reproduzindo um arquivo de áudio no JS
     // Passo 1: Copiar o arquivo áudio para o projeto
     // Passo 2: Usar a classe Áudio(biblioteca interna do JS)
     let som = new Audio ()
     som.src = "sound/glassbreaking.wav"
     som.play()
+    
 }
